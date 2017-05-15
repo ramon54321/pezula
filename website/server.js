6b48622f504e67dda6014163aa68a52067c50bf3
Ramon Brand <ramon2009@gmail.com>
2017-05-15 10:42:39 +0300

    Fixed index directory.

--- website/server.js
+++ website/server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 console.log("Running node app.");
 
 http.createServer(function(req, res){
-  fs.readFile('index.html',function (err, data){
+  fs.readFile('./bin/index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
