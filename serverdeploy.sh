1e6b34a6be6faeb5b569a4609c04410458219636
Ramon Brand <ramon2009@gmail.com>
2017-05-17 16:39:33 +0300

    Testing deployment error with package.json.

--- serverdeploy.sh
+++ serverdeploy.sh
@@ -1,4 +1,4 @@
 cd pezula
 git pull
 docker-compose down
-docker-compose -f docker-compose-production.yml up -d --remove-orphans
+docker-compose -f docker-compose-production.yml up -d --remove-orphans --build
