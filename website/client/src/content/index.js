22782f32a0d7b98fc8be657af094dd27b351f3a5
Ramon Brand <ramon2009@gmail.com>
2017-05-20 16:36:33 +0300

    Redone responsive design.

--- website/client/src/content/index.js
+++ website/client/src/content/index.js
@@ -27,6 +27,7 @@ export const Section3 = () => (
     <div className="pageSubSectionContainer">
       <div className="pageSectionTitle">Lorem Ipsum</div>
       <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
+      <p>Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi.</p>
       <div>
 
       </div>
